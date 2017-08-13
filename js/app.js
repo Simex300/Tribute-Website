3e4ce460a7dbee4bdd26b3a873fd48152d5be61a
PC\Adonis <adonis0444@gmail.com>
2017-08-13 12:17:13 -0400

    Change character display

--- js/app.js
+++ js/app.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
         scrollSpeed: 700,
         scrollbars: false,
     });
+
+    $(".carousel-control-next").parent().on('click', function(){
+        var actualTab = $("#character-tab a.active");
+        var firstTab = $("#character-tab a:first");
+        var lastTab = $("#character-tab a:last");
+        if($(actualTab).data('pos') == $(lastTab).data('pos')){
+            $(firstTab).tab('show');
+        }
+        else{
+            $(actualTab).parent().next().children().tab('show');
+        }
+    });
+
+    $(".carousel-control-prev").parent().on('click', function(){
+        var actualTab = $("#character-tab a.active");
+        var firstTab = $("#character-tab a:first");
+        var lastTab = $("#character-tab a:last");
+        if($(actualTab).data('pos') == $(firstTab).data('pos')){
+            $(lastTab).tab('show');
+        }
+        else{
+            $(actualTab).parent().prev().children().tab('show');
+        }
+    });
 });
